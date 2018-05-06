851170ac81bcef35c7ec31de4ac0a5c2eacb0e61
dezmound <profacorp@gmail.com>
2018-05-06 18:50:15 +0200

    Поправит баги при подключении к процессу

--- gdb-server.js
+++ gdb-server.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = new express();
 const gdb = new ngdbmi();
 
-app.put('/process/:pid/runtime-info', (req, res, next) => {
+app.put('/process/:pid/runtime-info', (req, res) => {
     gdb.command('attach', () => {
-        gdb.sendcommand('-data-disassemble -s $pc -e "$pc + ' + (req.query.bytes || '20') + '" -- 2', function (e) {
+        gdb.sendcommand('-data-disassemble -s $pc -e "$pc + ' + (req.query.bytes || '20') + '" -- 2', (e) => {
             gdb.command('detach', () => {
                 res.json(e).end();
             }, {target:req.params.pid});
         }, (e) => {
-            res.json(e);
+            // res.json(e).end();
         });
     }, {target:req.params.pid});
 });
